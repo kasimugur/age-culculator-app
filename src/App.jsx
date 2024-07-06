598612ad664f3f2f2398f06c63ab1927f78156df
kasimugur <kasiugur7@gmail.com>
2024-07-06 16:46:23 +0300

    first components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+function App() {
+
+  return (
+    <>
+      
+  Day
+  DD
+
+  Month
+  MM
+
+  Year
+  YYYY
+
+  -- years
+  -- months
+  -- days
+  {/* .attribution { font-size: 11px; text-align: center; }
+  .attribution a { color: hsl(228, 45%, 44%); } */}
+  <div className="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">kasım</a>.
+  </div>
+    </>
+  )
+}
+
+export default App
