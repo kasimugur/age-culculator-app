d701b221da78c317fc35fc07b75b21edca2b3119
kasimugur <kasiugur7@gmail.com>
2024-07-07 00:07:01 +0300

    updated2

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,36 @@ import { useEffect, useState } from "react";
 
 function App() {
 
-  const [inpYear, setInpYear] = useState()
-  const [inpMonth, setInpMonth] = useState()
-  const [inpDay, setInpDay] = useState()
+  const [inpYear, setInpYear] = useState('')
+  const [inpMonth, setInpMonth] = useState('')
+  const [inpDay, setInpDay] = useState('')
+  const [age, setAge] = useState(null)
   const [today, setToday] = useState(new Date())
 
   useEffect(() => {
     const timer = setInterval(() => {
       setToday(new Date());
-    }, 10000)
+    }, 100000)
     return () => {
       clearInterval(timer);
     }
   }, [])
 
-  const calculate = () => {
-    const inputDate = new Date(`${inpYear}-${inpMonth}-${inpDay}`)
+  const inputDate = new Date(`${inpYear}-${inpMonth}-${inpDay}`)
+  const dateMs = today.getTime() - inputDate.getTime()
+  const getYear = dateMs / (1000 * 3600 * 24 * 365)
+  const getDay = dateMs / (1000 * 3600 * 24)
+  const getMonth = today.getMonth() - inputDate.getMonth()
 
-    console.log(inputDate.getMonth())
+  const calculate = () => {
+
+    console.log(getMonth, "/", getYear, "/", getDay)
   }
-  
+
   useEffect(() => {
     inpDay,
-    inpMonth,
-    inpYear
+      inpMonth,
+      inpYear
   }, [])
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -37,7 +43,7 @@ function App() {
         <div className="container">
           {/* <h1>{calculate()} </h1> */}
           <div onSubmit={handleSubmit} className="container-input">
-            <span>Day<input
+            <span>Day <input
               value={inpDay}
               type="number"
               onChange={(e) => setInpDay(e.target.value)}
@@ -56,8 +62,8 @@ function App() {
           </div>
           <span>-- <button onClick={calculate}>dönüştür</button></span>
           <div className="box">
-            <h1>38</h1><span>years</span>
-            <h1>3</h1><span>months</span>
+            <h1>{getYear.toFixed()} </h1><span>years</span>
+            <h1>{getMonth.toFixed} </h1><span>months</span>
             <h1>26</h1><span>days</span>
           </div>
         </div>
